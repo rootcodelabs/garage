c1265b8c0e9bb21cd1759f3063eee30941ad4ad5
Charith Madushanka <charithwm@yahoo.com>
2020-11-05 18:55:13 +0530

    Add files via upload
    created jitsi web app

---
+++ jitsi-web-app/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { Jutsu } from 'react-jutsu'
+import "./App.css"
+
+const App = () => {
+  const [room, setRoom] = useState('')
+  const [name, setName] = useState('')
+  const [call, setCall] = useState(false)
+  const [password, setPassword] = useState('')
+
+  const handleClick = event => {
+    event.preventDefault()
+    if (room && name) setCall(true)
+  }
+
+  return (
+  <div className="App">{call ? (
+    <Jutsu
+      roomName={room}
+      displayName={name}
+      password={password}
+      onMeetingEnd={() => console.log('Meeting has ended')}
+      loadingComponent={<p>loading ...</p>}
+      errorComponent={<p>Oops, something went wrong</p>} containerStyles={{ width: window.innerWidth, height: window.innerHeight }} />
+  ) : (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height:'100vh' }}>
+      <h2 style={{ textAlign: 'center' }}>Create your Meeting</h2>
+      <div style={{ textAlign: 'center' }}>
+        <form>
+          <div style={{ marginBottom: 5 }}>
+            <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
+          </div>
+          <div style={{ marginBottom: 5 }}>
+            <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div style={{ marginBottom: 5 }}>
+            <input id='password' type='text' placeholder='Password (optional)' value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button onClick={handleClick} type='submit' style={{ marginTop: 10 }}>
+            Start / Join
+          </button>
+        </form>
+      </div>
+    </div>
+  )}</div>
+  )
+}
+
+export default App
