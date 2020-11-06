f8a31f2ac32484e5579e5cd14a0e0865d20b0228
CharithMadushanka <charithwm@yahoo.com>
2020-11-06 16:32:38 +0530

    updated

--- jitsi-web-app/src/App.js
+++ jitsi-web-app/src/App.js
@@ -14,36 +14,35 @@ const App = () => {
   }
 
   return (
-  // <div className="App">{call ? (
-  //   <Jutsu
-  //     roomName={room}
-  //     displayName={name}
-  //     password={password}
-  //     onMeetingEnd={() => console.log('Meeting has ended')}
-  //     loadingComponent={<p>loading ...</p>}
-  //     errorComponent={<p>Oops, something went wrong</p>} containerStyles={{ width: window.innerWidth, height: window.innerHeight }} />
-  // ) : (
-  //   <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height:'100vh' }}>
-  //     <h2 style={{ textAlign: 'center' }}>Create your Meeting</h2>
-  //     <div style={{ textAlign: 'center' }}>
-  //       <form>
-  //         <div style={{ marginBottom: 5 }}>
-  //           <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
-  //         </div>
-  //         <div style={{ marginBottom: 5 }}>
-  //           <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
-  //         </div>
-  //         <div style={{ marginBottom: 5 }}>
-  //           <input id='password' type='text' placeholder='Password (optional)' value={password} onChange={(e) => setPassword(e.target.value)} />
-  //         </div>
-  //         <button onClick={handleClick} type='submit' style={{ marginTop: 10 }}>
-  //           Start / Join
-  //         </button>
-  //       </form>
-  //     </div>
-  //   </div>
-  // )}</div>
-  <h1>Hello World</h1>
+  <div className="App">{call ? (
+    <Jutsu
+      roomName={room}
+      displayName={name}
+      password={password}
+      onMeetingEnd={() => console.log('Meeting has ended')}
+      loadingComponent={<p>loading ...</p>}
+      errorComponent={<p>Oops, something went wrong</p>} containerStyles={{ width: window.innerWidth, height: window.innerHeight }} />
+  ) : (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height:'100vh' }}>
+      <h2 style={{ textAlign: 'center' }}>Create your Meeting</h2>
+      <div style={{ textAlign: 'center' }}>
+        <form>
+          <div style={{ marginBottom: 5 }}>
+            <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
+          </div>
+          <div style={{ marginBottom: 5 }}>
+            <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div style={{ marginBottom: 5 }}>
+            <input id='password' type='text' placeholder='Password (optional)' value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button onClick={handleClick} type='submit' style={{ marginTop: 10 }}>
+            Start / Join
+          </button>
+        </form>
+      </div>
+    </div>
+  )}</div>
   )
 }
 
